88df150714a76afecf613708a1e11960991e4ae5
Jmcclain0129 <jmcclain0129@gmail.com>
2019-08-04 11:15:51 +0100

    removed dependencies and reinstalled the requirements.txt file

--- app.py
+++ app.py
@@ -5,8 +5,15 @@ from bson.objectid import ObjectId
 
 
 app = Flask(__name__)
+
+app.route('/')
+def hello():
+    return 'Hello World ...again'
+
 app.config["MONGO_DBNAME"] = 'task_manager'
-app.config["MONGO_URI"] = os.getenv('MONGO_URI')
+# app.config["MONGO_URI"] = os.getenv('MONGO_URI')
+#
+app.config["MONGO_URI"]="mongodb+srv://root:<Root123>@myfirstcluster-843eu.mongodb.net/test?retryWrites=true&w=majority"
 
 mongo = PyMongo(app)
 
@@ -95,8 +102,13 @@ def insert_category():
 def add_category():
     return render_template('addcategory.html')
 
-
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
+    app.run(host=os.environ.get('IP', '127.0.0.1'),
+            port=int(os.environ.get('PORT', '8080')),
             debug=True)
+
+
+# if __name__ == '__main__':
+#     app.run(host=os.environ.get('IP'),
+#             port=int(os.environ.get('PORT')),
+#             debug=True)
